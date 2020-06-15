b219062fcb06573f5c45932575bdedce047e7f25
Donghyeon Choi <drea123@naver.com>
2020-06-15 14:49:19 +0900

    fix: edit ga mode

--- client/src/constants/ga.ts
+++ client/src/constants/ga.ts
@@ -1,7 +1,8 @@
 import ReactGA from 'react-ga';
 
 const trackingId = "UA-164242824-13"; 
-const GA_FLAG = (process.env.GA_FLAG === 'true') || true;
-ReactGA.initialize(trackingId, { titleCase: false, debug: (!GA_FLAG) });
+const GA_FLAG = true;
+
+ReactGA.initialize(trackingId, { titleCase: false, testMode: (!GA_FLAG) });
 
 export default ReactGA;
